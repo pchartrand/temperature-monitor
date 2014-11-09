a9aecd85f76be714c81122d1506931063e35377a
Philippe Chartrand <philippe.chartrand@videotron.ca>
2014-11-09 12:24:53 -0500

    unit testing of TempDataset class

--- libs/tempseries.py
+++ libs/tempseries.py
@@ -1,12 +1,14 @@
-import numpy as np
-
-
 class TempDataset(object):
     def __init__(self, temperature_time_series):
         self.temperature_time_series = temperature_time_series
         self.timestamps, self.temperatures = self.split_coordinates_to_distinct_series(temperature_time_series)
-        self.average_temperature = np.average(self.temperatures)
+        self.average_temperature = self.get_average_temperature()
 
+    def get_average_temperature(self):
+        if len(self.temperatures):
+            return float(reduce(lambda x,y: float(x)+float(y), self.temperatures)) / len(self.temperatures)
+        else:
+            return None
     @staticmethod
     def split_coordinates_to_distinct_series(s):
         x = []
---
+++ tests/libs/tests_tempseries.py
@@ -0,0 +1,34 @@
+from nose.tools import istest
+
+from unittest import TestCase
+
+from libs.tempseries import TempDataset
+
+
+class TemDatasetTests(TestCase):
+    @istest
+    def can_split_coordinate_series_in_distinct_series(self):
+        dataset = TempDataset([[1,1.1], [2, 1.2], [3, 1.3]])
+
+        self.assertEqual([1, 2, 3], dataset.timestamps)
+        self.assertEqual([1.1, 1.2, 1.3], dataset.temperatures)
+
+    @istest
+    def can_calculate_average_temperature(self):
+        dataset = TempDataset([[1,1.1], [2, 1.2], [3, 1.3]])
+
+        self.assertEqual(1.2, dataset.get_average_temperature())
+
+    @istest
+    def constructor_calculates_average_temperature(self):
+        dataset = TempDataset([[1,1.1], [2, 1.2], [3, 1.3]])
+
+        self.assertEqual(1.2, dataset.average_temperature)
+
+    @istest
+    def dataset_behaves_well_when_no_serie_is_given(self):
+        dataset = TempDataset([])
+
+        self.assertEqual([], dataset.timestamps)
+        self.assertEqual([], dataset.temperatures)
+        self.assertIsNone(dataset.average_temperature)
