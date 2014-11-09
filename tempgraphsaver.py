77cb04697dc0d8adf781e7b7db64c106a40f5343
Philippe Chartrand <philippe.chartrand@videotron.ca>
2014-11-09 10:21:51 -0500

    Allow graphing without X11. Define output directory.

--- tempgraphsaver.py
+++ tempgraphsaver.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 #- coding: utf-8 -#
+import logging
+import os
+
+import matplotlib
+matplotlib.use('Agg')  #graphical backend not requiring X11
 import matplotlib.pyplot as plt
 from pylab import savefig, setp
 
-
+from libs.constants import GRAPHS_OUTPUT_DIRECTORY
 from libs.store import Store
 from libs.storeseriesfetcher import StoreSeriesFetcher
 from libs.tempseriesplot import plot_temperatures
 from libs.templib import get_time
+
+logging.basicConfig(level=logging.INFO)
 
 def resize_xticklabels(plt, fontsize=10):
     setp(plt.axes().get_xticklabels(), fontsize=fontsize)
@@ -21,5 +28,6 @@ if __name__ == '__main__':
     resize_xticklabels(plt, fontsize=8)
     filename = 'temperatures_%s.png' % get_time()
     filename = filename.replace(' ', '_')
-    print("saving to file %s" % filename)
+    filename = os.path.join(GRAPHS_OUTPUT_DIRECTORY, filename)
+    logging.info("saving to file %s", filename)
     savefig(filename, dpi=300)
