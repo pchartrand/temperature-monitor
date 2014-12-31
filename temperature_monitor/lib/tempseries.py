bd430ef04eb631554a4907e3c6f8b06f6aa758aa
Philippe Chartrand <philippe.chartrand@videotron.ca>
2014-12-30 21:36:32 -0500

    cleanup of tempseries properties

--- temperature_monitor/lib/tempseries.py
+++ temperature_monitor/lib/tempseries.py
@@ -9,37 +9,44 @@ class TempDataset(object):
     def __init__(self, temperature_time_series):
         self.timestamps, self.temperatures = self.split_coordinates_to_distinct_series(temperature_time_series)
         self.len = len(self.temperatures)
-        self.average_temperature = self.get_average_temperature()
-        self.temperature_variation = self.get_total_temperature_variation()
-        self.time_variation_in_seconds = self.get_time_variation_in_seconds()
+        self.current_temperature = self._get_current_temperature()
+        self.average_temperature = self._get_average_temperature()
+        self.temperature_variation = self._get_total_temperature_variation()
+        self.temperature_variation_for_last_hour = self._get_temperature_variation_for_last_hour()
+        self.time_variation = self._get_time_variation()
+        self.time_variation_in_seconds = self._get_time_variation_in_seconds()
 
-    def get_average_temperature(self):
+    def _get_average_temperature(self):
         if self.len > 0:
             return reduce(lambda x, y: x + y, self.temperatures) / self.len
         else:
             return None
 
-    def get_total_temperature_variation(self):
+    def _get_total_temperature_variation(self):
         if self.len > 0:
             return round(self.temperatures[-1] - self.temperatures[0], 1)
 
-    def get_time_variation(self):
-        if self.len > 0:
-            return self.timestamps[-1] - self.timestamps[0]
+    def _get_temperature_variation_for_last_hour(self):
+        if self.len >= 60:
+            return round(self.temperatures[-1] - self.temperatures[-60], 1)
 
-    @staticmethod
-    def _convert_datetime_to_seconds(value):
-        return value.strftime('%s') if isinstance(value, datetime.datetime) else value
-
-    def get_time_variation_in_seconds(self):
+    def _get_time_variation_in_seconds(self):
         if self.len > 0:
             t0 = long(self._convert_datetime_to_seconds(self.timestamps[0]))
             t1 = long(self._convert_datetime_to_seconds(self.timestamps[-1]))
             return t1 - t0
 
-    def get_temperature_variation_for_last_hour(self):
-        if self.len >= 60:
-            return round(self.temperatures[-1] - self.temperatures[-60], 1)
+    def _get_time_variation(self):
+        if self.len > 0:
+            return self.timestamps[-1] - self.timestamps[0]
+
+    def _get_current_temperature(self):
+        if self.len > 0:
+            return self.temperatures[-1]
+
+    @staticmethod
+    def _convert_datetime_to_seconds(value):
+        return value.strftime('%s') if isinstance(value, datetime.datetime) else value
 
     @staticmethod
     def split_coordinates_to_distinct_series(s):
--- temperature_monitor/tests/tests_tempseries.py
+++ temperature_monitor/tests/tests_tempseries.py
@@ -24,22 +24,57 @@ class TemDatasetTests(TestCase):
         )
         self.assertEqual([1.1, 1.2, 1.3], dataset.temperatures)
 
-    istest
+    @istest
     def len_property_keeps_track_of_number_of_measureements(self):
         dataset = TempDataset([[1,1.1], [2, 1.2], [3, 1.3]])
         self.assertEqual(3, dataset.len)
 
     @istest
-    def can_calculate_average_temperature(self):
+    def calculates_current_temperature(self):
         dataset = TempDataset([[1,1.1], [2, 1.2], [3, 1.3]])
 
-        self.assertEqual(1.2, dataset.get_average_temperature())
+        self.assertEqual(1.3, dataset.current_temperature)
 
     @istest
-    def constructor_calculates_average_temperature(self):
+    def calculates_average_temperature(self):
         dataset = TempDataset([[1,1.1], [2, 1.2], [3, 1.3]])
 
         self.assertEqual(1.2, dataset.average_temperature)
+
+    @istest
+    def calculates_temperature_variation(self):
+        dataset = TempDataset([
+            [datetime.datetime(2014, 12, 26, 15, 6, 18), 12.1],
+            [datetime.datetime(2014, 12, 26, 15, 6, 19), 11.2],
+            [datetime.datetime(2014, 12, 26, 15, 6, 20), 11.1]
+        ])
+        self.assertEqual(-1.0, dataset.temperature_variation)
+
+    @istest
+    def calculates_temperature_variation_in_last_hour(self):
+        times = [datetime.datetime(2014, 12, 26, 15, 6, i) for i in range(60)]
+        measurements = [float(i) for i in range(60)]
+        dataset = TempDataset(zip(times, measurements))
+
+        self.assertEqual(59, dataset.temperature_variation_for_last_hour)
+
+    @istest
+    def calculates_time_variation(self):
+        dataset = TempDataset([
+            [datetime.datetime(2014, 12, 26, 15, 6, 18), 12.1],
+            [datetime.datetime(2014, 12, 26, 15, 6, 19), 11.2],
+            [datetime.datetime(2014, 12, 26, 15, 6, 20), 11.1]
+        ])
+        self.assertEqual(datetime.timedelta(0, 2), dataset.time_variation)
+
+    @istest
+    def calculates_time_variation_in_seconds(self):
+        dataset = TempDataset([
+            [datetime.datetime(2014, 12, 26, 15, 6, 18), 12.1],
+            [datetime.datetime(2014, 12, 26, 15, 6, 19), 11.2],
+            [datetime.datetime(2014, 12, 26, 15, 6, 20), 11.1]
+        ])
+        self.assertEqual(2L, dataset.time_variation_in_seconds)
 
     @istest
     def dataset_behaves_well_when_no_serie_is_given(self):
@@ -47,14 +82,15 @@ class TemDatasetTests(TestCase):
 
         self.assertEqual([], dataset.timestamps)
         self.assertEqual([], dataset.temperatures)
+        self.assertEqual(0, dataset.len)
+        self.assertIsNone(dataset.current_temperature)
         self.assertIsNone(dataset.average_temperature)
+        self.assertIsNone(dataset.temperature_variation)
+        self.assertIsNone(dataset.temperature_variation_for_last_hour)
+        self.assertIsNone(dataset.time_variation)
+        self.assertIsNone(dataset.time_variation_in_seconds)
 
-    @istest
-    def can_get_temperature_variation_and_time_variation(self):
-        dataset = TempDataset([
-            [datetime.datetime(2014, 12, 26, 15, 6, 18), 12.1],
-            [datetime.datetime(2014, 12, 26, 15, 6, 19), 11.2],
-            [datetime.datetime(2014, 12, 26, 15, 6, 20), 11.1]
-        ])
-        self.assertEqual(2L, dataset.time_variation_in_seconds)
-        self.assertEqual(-1.0, dataset.temperature_variation)
+
+
+
+
